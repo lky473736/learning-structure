275a021a8e083580947e92cb00aa02ff5dc17e12
lky473736 <lky473736@icloud.com>
2024-03-26 10:55:06 +0900

    linked list original

--- topic4/linked_list.c
+++ topic4/linked_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct list {
     char name[10]; // data field
@@ -8,7 +9,7 @@ struct list {
 };
 
 int main() {
-    struct list *lst, *prev;
+    struct list *lst, *prev, *head;
     
     struct list *lst_address[3];
     
@@ -20,10 +21,11 @@ int main() {
         
         printf ("%d번째 node를 입력 : ", i+1);
         scanf ("%s %d", lst->name, &(lst->no));
-        printf ("%s %d\n", lst->name, lst->no);
+        // printf ("%s %d\n", lst->name, lst->no);
     
         if (prev == NULL) {
             prev = lst;
+            head = lst;
         }
         
         else {
@@ -36,13 +38,28 @@ int main() {
         }
     }
     
-    for (int i = 0; i < 3; i++) {
-        printf ("%s %d %p\n", lst_address[i]->name, lst_address[i]->no, lst_address[i]->link);
-    }
+    // for (int i = 0; i < 3; i++) {
+    //     printf ("%s %d %p\n", lst_address[i]->name, lst_address[i]->no, lst_address[i]->link);
+    // }
     
-    for (int i = 0; i < 3; i++) {
-        printf ("%s %d %p deleted!\n", lst_address[i]->name, lst_address[i]->no, lst_address[i]->link);
-        free(lst_address[i]);
+    // for (int i = 0; i < 3; i++) {
+    //     printf ("%s %d %p deleted!\n", lst_address[i]->name, lst_address[i]->no, lst_address[i]->link);
+    //     free(lst_address[i]);
+    // }
+    
+    while (true) {
+        struct list *p;
+        
+        if (head == NULL) {
+            break;
+        }
+        
+        // printf ("%s %d %p\n", head->name, head->no, head->link);
+        p = head;
+        head = head->link;
+        
+        printf ("%s %d %p deleted!\n", p->name, p->no, p->link);
+        free(p);
     }
     
     return 0;
